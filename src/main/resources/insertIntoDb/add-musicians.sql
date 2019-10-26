29eb495d85dba8ee82cb967da697614c2052d755
jurenivan <juren.ivan@gmail.com>
2019-10-26 10:14:07 +0200

    add sql to fill db with few musicians for testing purpouses

---
+++ src/main/resources/insertIntoDb/add-musicians.sql
@@ -0,0 +1,8 @@
+select * from band;
+
+insert into musician(id, instrument, name) values (1,'guitar','john1');
+insert into musician(id, instrument, name) values (2,'piano','john2');
+insert into musician(id, instrument, name) values (3,'bass','john3');
+insert into musician(id, instrument, name) values (4,'drums','john4');
+
+select * from band;
