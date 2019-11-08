a5666d2800937072bc806bfd5ee40800cfebc3d7
mihael nosil <nosil.mihael@gmail.com>
2019-11-08 09:45:51 +0100

    added header, changed landing page

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Header from "./components/Header";
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const [loadingUser, setLoadingUser] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("/user")
+      .then(response => {
+         if (response.status !== 401) {
+           setLoadingUser(false);
+           setIsLoggedIn(true);
+         } else {
+           setLoadingUser(false);
+         }
+       })
+  }, []);
+
+  if (loadingUser) {
+    return <div>Loading...</div>
+  }
+
+  function onLogin() {
+    setIsLoggedIn(true)
+  }
+
+  function onLogut() {
+    setIsLoggedIn(false);
+  }
+
+  if (!isLoggedIn) {
+    return (
+      <div className="App">
+        <Login onLogin={onLogin}/>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header onLogout={onLogut}/>
+      <div className="App">
+        <Switch>
+          <Route path='/Logout' exact component={Login}/>
+          <Route path='/Login' exact component={Login}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ web/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+  import {Link} from "react-router-dom";
+  import "./Header.css";
+
+function Header(props) {
+
+  function logout() {
+    fetch("/logout").then(() => {
+      props.onLogout();
+    });
+  }
+
+  return (
+    <header className="Header">
+      <Link to='/events'>Events</Link>
+      <Link to='/bands'>Bands</Link>
+      <Link to='/login'>login</Link>
+      <button onClick={logout}>Logout</button>
+    </header>
+  )
+}
+
+export default Header;
