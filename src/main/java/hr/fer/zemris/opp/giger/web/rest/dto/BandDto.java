af60ecf2017dfd348ae0680b69581c7ac21f5307
jurenivan <juren.ivan@gmail.com>
2019-12-23 04:15:45 +0100

    rename to meet convetion

--- src/main/java/hr/fer/zemris/opp/giger/web/rest/dto/BandDto.java
+++ src/main/java/hr/fer/zemris/opp/giger/web/rest/dto/BandDto.java
@@ -10,11 +10,10 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class BandPreviewDto {
+public class BandDto {
 
     private long id;
     private String name;
     private String pictureURl;
     private List<GigType> gigTypes;
-
 }
--- src/main/java/hr/fer/zemris/opp/giger/web/rest/dto/CommentDto.java
+++ src/main/java/hr/fer/zemris/opp/giger/web/rest/dto/CommentDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommentPreviewDto {
+public class CommentDto {
 
     private Long id;
     private String content;
