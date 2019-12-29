ed8419580fcac74b72efb240c0b68c0a20c26429
Stela Gaši <sg50985@fer.hr>
2019-12-29 14:11:41 +0100

    added missing annotations

--- src/main/java/hr/fer/zemris/opp/giger/repository/OccasionRepository.java
+++ src/main/java/hr/fer/zemris/opp/giger/repository/OccasionRepository.java
@@ -2,7 +2,9 @@ package hr.fer.zemris.opp.giger.repository;
 
 import hr.fer.zemris.opp.giger.domain.Occasion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OccasionRepository extends JpaRepository<Occasion, Long> {
 
 }
