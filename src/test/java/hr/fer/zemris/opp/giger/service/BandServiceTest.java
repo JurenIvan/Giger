6f116729e91f1c2b99040827814d042da165e378
jurenivan <juren.ivan@gmail.com>
2020-01-04 19:10:48 +0100

    work in progress

--- src/test/java/hr/fer/zemris/opp/giger/service/BandServiceTest.java
+++ src/test/java/hr/fer/zemris/opp/giger/service/BandServiceTest.java
@@ -1,13 +1,21 @@
 package hr.fer.zemris.opp.giger.service;
 
 import hr.fer.zemris.opp.giger.config.security.UserDetailsServiceImpl;
+import hr.fer.zemris.opp.giger.domain.Band;
+import hr.fer.zemris.opp.giger.domain.Musician;
+import hr.fer.zemris.opp.giger.domain.exception.GigerException;
 import hr.fer.zemris.opp.giger.repository.*;
+import hr.fer.zemris.opp.giger.web.rest.dto.MusicianBandDto;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.mockito.Mockito.when;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -28,6 +36,14 @@ public class BandServiceTest {
 	@InjectMocks
 	BandService bandService;
 
+	@Mock
+	private Musician musician1;
+	@Mock
+	private Musician musician2;
+	@Mock
+	private Band band;
+
+
 	@Before
 	public void setUp() {
 	}
@@ -36,8 +52,31 @@ public class BandServiceTest {
 	public void createBand() {
 	}
 
+	@Test(expected = GigerException.class)
+	public void inviteMusician_noSuchBand() {
+		when(bandRepository.findById(1L)).thenReturn(empty());
+
+		MusicianBandDto musicianBandDto = new MusicianBandDto();
+		musicianBandDto.setMusicianId(1);
+		musicianBandDto.setBandId(1);
+
+		bandService.inviteMusician(musicianBandDto);
+	}
+
 	@Test
-	public void inviteMusician() {
+	public void inviteMusician_assertCreated() {
+		when(bandRepository.findById(1L)).thenReturn(of(band));
+		when(band.getLeader()).thenReturn(musician2);
+		when(musician2.getId()).thenReturn(1L);
+		when(userDetailsService.getLoggedMusician()).thenReturn(musician2);
+
+		when(musicianRepository.findById(2L));
+
+		MusicianBandDto musicianBandDto = new MusicianBandDto();
+		musicianBandDto.setMusicianId(1);
+		musicianBandDto.setBandId(1);
+
+		bandService.inviteMusician(musicianBandDto);
 	}
 
 	@Test
