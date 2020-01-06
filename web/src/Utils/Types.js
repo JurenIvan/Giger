9c6293bf3ae2269497db76f64c16ae689f38e514
paolo <paolo.lanca@fer.hr>
2020-01-06 16:18:52 +0100

    Changed endpoint for viewing gig

--- web/src/Utils/Types.js
+++ web/src/Utils/Types.js
@@ -5,10 +5,10 @@ export const endpoints = {
     CREATE_MUSICIAN: "/musicians/create",
     CREATE_ORGANIZER: "/organizers/create/",
     GET_BAND_ID: "/bands/like/",
-    GET_MY_GIGS: "/gigs/",
+    GET_MY_GIGS: "/gigs/view/",
     INVITE_TO_GIG: "/gigs/invite",
     GET_BAND_GIGS: "/bands/invitations/",
-    GET_GIG: "/gigs/",
+    GET_GIG: "/gigs/view/",
     ACCEPT_GIG: "/bands/invitations/accept",
     CANCEL_GIG: "/bands/invitations/cancel",
     GET_BANDS_LEAD: "/bands/my/leader"
